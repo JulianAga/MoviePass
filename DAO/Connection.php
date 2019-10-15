dc2f5a50cac990303b7fb514a2e1800efdae3150
Nicolas Vezzali dussio <nico_dussio22@hotmail.com>
2019-10-15 10:14:21 -0300

    archivos basicos para BD

---
+++ DAO/Connection.php
@@ -0,0 +1,12 @@
+<?php namespace DAOS;
+
+use \Exception as Exception;
+
+    class Connection {
+        
+        public function Connect() 
+        {
+            return new \PDO("mysql:host=" . DB_HOST . "; dbname=" . DB_NAME, DB_USER, DB_PASS);
+        }
+    }
+?>
