b983daf245c63e0aff0d1c2aa18f4b58b13f9f57
Nicolas Vezzali dussio <nico_dussio22@hotmail.com>
2019-10-23 12:02:12 -0300

    GenerosDAO
    al recibir peliculas en home ADM , tambien se guardan los generos en BD

---
+++ DAO/GenerosDAO.php
@@ -0,0 +1,78 @@
+<?php namespace DAO;
+
+
+
+use \Exception as Exception;
+use \PDOException as PDOException;
+
+/**
+ * 
+ */
+class GenerosDAO extends SingletonAbstractDAO implements IDAO
+{
+	private $table = 'Generos';
+	
+	
+
+	public function insertar($dato){
+		try 
+    	{
+    		
+
+			$query = 'INSERT INTO '.$this->table.' 
+			(id_genero,nombre_genero) 
+			VALUES 
+			(:id_genero,:nombre_genero)';
+
+			$pdo = new Connection();
+			$connection = $pdo->Connect();
+			$command = $connection->prepare($query);
+
+			$id_genero = $dato->getId();
+			$nombre_genero = $dato->getName();
+			
+
+			$command->bindParam(':nombre_genero', $nombre_genero);
+			$command->bindParam(':id_genero', $id_genero);
+			$command->execute();
+
+    	}
+    	catch (PDOException $ex) {
+			throw $ex;
+    	}
+    	catch (Exception $e) {
+			throw $e;
+    	}
+
+	}//FIN INSERTAR
+	//
+	//
+	//
+	public function guardarGeneros(){
+
+
+	}
+	//
+	//
+	//
+	public function insertarDevolverID($dato){
+
+	}
+	// Busca en la tabla la fila con el mismo ID pasado por parametros, y lo retorna en forma de objeto.
+	public function buscarPorID($dato){
+
+	}
+	// Busca en la tabla la fila con el mismo ID pasado por parametros, y lo borra. No retorna nada.
+	public function borrar($dato){
+
+	}
+	// Recibe un objeto (ya modificado) por parametros y lo reemplaza en la tabla y lo actualiza.
+	public function actualizar($dato){
+
+	}
+	// Retorna todas las filas de la tabla en forma de objetos en un array.
+	public function traerTodos(){
+
+	}
+}
+?>
